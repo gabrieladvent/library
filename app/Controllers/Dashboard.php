64c396c78137f1876f36172f017656c24e68ef9d
CHELVYN01 <kledenkelvin9@gmail.com>
2024-12-10 15:23:00 +0700

    build controller/dashboard : Write down new features that need to be worked on

---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Dashboard extends BaseController
+{
+    public function dashboardView()
+    {
+        // menampilkan data jumalah peminjam baru (int) : count_newborrower
+        // menampilkan jumlah buku yang ada di perpustakaan (int) : count_book
+        // menampilkan jumalh user (int) : count_member 
+    }
+}
