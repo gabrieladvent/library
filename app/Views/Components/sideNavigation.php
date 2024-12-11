4d2b2eed1416465d0cd4257c82c3e47f1c684858
CHELVYN01 <kledenkelvin9@gmail.com>
2024-12-11 12:51:49 +0700

    feat/dashboar:creae style.css

---
+++ app/Views/Components/sideNavigation.php
@@ -0,0 +1,16 @@
+<?php
+
+use PharIo\Manifest\Url;
+?>
+
+<head>
+    <link rel="stylesheet" href="<?= base_url('css/style.sidebar.css') ?>">
+</head>
+
+<body>
+
+    <main>
+        <h1>ini sidebar</h1>
+    </main>
+
+</body>
