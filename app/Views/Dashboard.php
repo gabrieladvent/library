916eac9f0311af406b32d3ef31c34062a31e23a2
CHELVYN01 <kledenkelvin9@gmail.com>
2024-12-14 18:28:44 +0700

    feat/buku

--- app/Views/Dashboard.php
+++ app/Views/Dashboard.php
@@ -5,14 +5,19 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>sidebar</title>
-    <link rel="stylesheet" href="<?= base_url('css/style.sidebar.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('css/style.dashboard.css') ?>" />
 
 </head>
 
 <body>
 
     <?= view("Components/navbar") ?>
-    <?= view("Components/sideNavigation") ?>
+    <div class="container">
+        <?= view("Components/sideNavigation") ?>
+        <div>
+            <h1>latihan</h1>
+        </div>
+    </div>
 
 
 </body>
